86a50582973ef9a3fd48de0b550df50f3ff2bdad
JM_96 <215783@unsaac.edu.pe>
2023-07-05 10:16:14 -0500

    Creacion de la BibliotecaTDA

---
+++ BibliotecaTDA/cListaIterativa.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaTDA
+{
+    internal class cListaIterativa
+    {
+        //Attributes
+        private cNodo? aFirst;
+
+        //Constructors
+        cListaIterativa()
+        {
+            aFirst = null;
+        }
+
+        cListaIterativa(cNodo? pFirst)
+        {
+            aFirst = pFirst;
+        }
+
+        //Properties
+
+        public cNodo? First
+        {
+            get { return aFirst; }
+            set { aFirst = value; }
+        }
+
+        //Metods
+
+        public bool EsVacio()
+        {
+            return aFirst == null;
+        }
+
+        public void Listar()
+        {
+            cNodo? aAux = aFirst;
+            Console.WriteLine("LISTA DE ELEMENTOS:");
+            Console.WriteLine("-----------------------");
+            while (aAux.Next != null)
+            {
+                Console.WriteLine("- " + aAux.Elemento);
+            }    
+        }
+
+        public object? Iesimo(int pos)
+        {
+            cNodo? aAux = aFirst;
+            int cont = 0;
+            while(cont != pos)
+            {
+                aAux = aAux.Next; 
+                cont++;
+            }
+            return null;
+        }
+
+        public void Agregar(object pElemento)
+        {
+            cNodo? aAux = aFirst;
+            while(aAux.Next != null)
+            {
+                aAux = aAux.Next;
+            }
+            aAux.Next = new cNodo(pElemento);
+        }
+
+        public void Insertar(object pElemento, int pos)
+        {
+            cNodo? aAux = aFirst;
+            int cont = 0;
+            while( cont != pos)
+            {
+                aAux = aAux.Next;
+                cont++;
+            }
+            aAux.Next = new cNodo(pElemento,aAux.Next.Next);
+        }
+
+        public void Eliminar(int pos)
+        {
+
+        }
+
+        public void Eliminar(object pElemento)
+        {
+            
+        }
+
+        public int Ubicacion(object pElemento)
+        {
+            return 0;
+        }
+
+        public int Longitud()
+        {
+            cNodo? aAux = aFirst;
+            int Long = 0;
+            while (aAux != null)
+            {
+                Long += 1;
+                aAux = aAux.Next;
+            }
+            return Long;
+        }
+    }
+}
---
+++ BibliotecaTDA/cNodo.cs
@@ -0,0 +1,55 @@
+﻿namespace BibliotecaTDA
+{
+    public class cNodo
+    {
+        //Attributes
+
+        private object? aElemento;
+        private cNodo? aNext;
+
+        //Constructors
+
+        public cNodo()
+        {
+            aElemento = null;
+            aNext = null;
+        }
+
+        public cNodo(object pElemento, cNodo pNext)
+        {
+            aElemento = pElemento;
+            aNext = pNext;
+        }
+
+        public cNodo(object pElemento)
+        {
+            aElemento = pElemento;
+            aNext = null;
+        }
+
+        //Properties
+        
+        public object? Elemento
+        {
+            get { return aElemento; }
+            set { aElemento = value; }
+        }
+
+        public cNodo? Next
+        {
+            get { return aNext; }
+            set { aNext = value; }
+        }
+
+        //Metods
+
+        public bool EsVacio()
+        {
+            if (aElemento == null && aNext == null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
