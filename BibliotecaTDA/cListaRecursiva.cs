7577286e50a6838b316c3a62e6988ef417d438ab
ManjaroUser1 <alandelgadorocio@gmail.com>
2023-07-16 20:23:43 -0500

    Modificando y agregando Lista Ordenada y recursiva 1.0

---
+++ BibliotecaTDA/cListaRecursiva.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace BibliotecaTDA
+{
+    public class cListaRecursiva
+    {
+        private cListaRecursiva? aSublista;
+        private object? aObjeto;
+
+        public cListaRecursiva()
+        {
+            aObjeto = null;
+            aSublista = null;
+        }
+
+        public cListaRecursiva(cListaRecursiva pSublista, object pObjeto)
+        {
+            aObjeto = pObjeto;
+            aSublista = pSublista;
+        }
+
+        public cListaRecursiva(object pObjeto) {
+            aObjeto = pObjeto;
+            aSublista = null;
+        }
+        //Properties
+
+        public object? Objeto {
+            get { return aObjeto; }
+            set { aObjeto = value; }
+        }
+
+        public cListaRecursiva? SubLista {
+            get { return aSublista;}
+            set { aSublista = value; }
+        }
+
+        public bool isEmpty()
+        {
+            return aSublista==null && aObjeto==null;
+        }
+
+        public int Longitud(){
+            if (aSublista==null){
+                return 0;
+            }
+            else {
+                return 1 + aSublista.Longitud();
+            }
+        }
+        public cListaRecursiva Agregar(object pObjeto)
+        {
+            if( aSublista == null){
+                aObjeto = pObjeto;
+                aSublista = new cListaRecursiva(); 
+                return aSublista;
+            }
+            else{
+                return aSublista.Agregar(pObjeto);
+            }
+        }
+
+        public int Ubicacion(object pObjeto){
+           
+           if ( aObjeto.Equals(pObjeto)){
+                return 0;
+            }
+            else {
+                return 1 + aSublista.Ubicacion(pObjeto);
+            }
+        }
+
+        public object? Iesimo( int pos){
+            
+            if (pos == 0) {
+                return aObjeto;
+            }
+            else {
+                return aSublista.Iesimo(--pos);
+            }
+        }
+
+        public void Eliminar(int pos) {
+            if (pos == 0 && pos < Longitud()){
+                aSublista = aSublista.SubLista;
+            }
+            else {
+                aSublista.Eliminar(--pos);
+            }
+        }
+        public void Listar(){
+            if (aSublista == null){
+                return;
+            }
+            else {
+                Console.WriteLine($"- {aObjeto}");
+                aSublista.Listar();
+            }
+        }
+    }
+}
