d64f2e4026e8ecb6367db99289edb1a90afefe47
JM_96 <215783@unsaac.edu.pe>
2023-07-19 09:26:50 -0500

    Creacion de la AppCalculadora-Convertidor&StringToKenizer

---
+++ AppCalculadora/cConvertidorAPosfijo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCalculadora
+{
+    internal class cConvertidorAPosfijo
+    {
+        //Attributes
+
+        private string aExpresion;
+
+        //Constructors
+
+        public cConvertidorAPosfijo()
+        {
+            aExpresion = string.Empty;
+        }
+
+        public cConvertidorAPosfijo(string pExpresion)
+        {
+            aExpresion = pExpresion;
+        }
+
+        public static cConvertidorAPosfijo Crear(string pExpresion)
+        {
+            return new cConvertidorAPosfijo(pExpresion);
+        }
+
+        //Properties
+
+        public string Expresion
+        {
+            get { return aExpresion; }
+            set { aExpresion = value; }
+        }
+
+        //Metods
+
+        public bool okPrecedencia(string Token1,string Token2)
+        {
+            return false;
+        }
+    }
+}
